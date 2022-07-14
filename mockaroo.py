5a196be684a60942c7317da0341b5278df464a05
Paddy Gorry <paddy.gorry@mu.ie>
2022-07-14 19:21:36 +0100

    Added mockaroo examples and tests

--- mockaroo.py
+++ mockaroo.py
@@ -2,5 +2,9 @@ import geopandas as gpd
 import pandas as pd
 import csv
 
+for i in range(1,11):
+    source = pd.read_csv('mockaroo/mockaroo_points/mockaroo{}.csv'.format(i))
+    source_gdf = gpd.GeoDataFrame(source, geometry=gpd.points_from_xy(source.lon, source.lat))
+    source_gdf.to_file('mockaroo/mockaroo_points/mockaroo{}.geojson'.format(i), driver='GeoJSON')
 
 
