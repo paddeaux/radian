00b03daeaa857886582688f26fc6366251af04a2
paddeaux <gorrypaddy@gmail.com>
2023-06-10 14:38:54 +0100

    Re-running tests

--- radian.py
+++ radian.py
@@ -54,8 +54,8 @@ def poly_bb_ratio(poly):
     min_x, min_y, max_x, max_y = poly.bounds
     bb = gpd.GeoSeries(box(min_x, min_y, max_x, max_y, ccw=True))
     ratio = float(1/(poly.area/bb.area))
-    if ratio < 2:
-        return 2
+    if ratio < 1.8:
+        return 1.8
     return ratio
 
 ########## POINT GENERATION ##########
@@ -892,7 +892,6 @@ def radian_uniform(total_pts, source):
 
     return time_taken 
 
-def csv_to_geojson()
 
 def uniform_benchmark(filepath, iterations):
     print("Reading {}...".format(filepath))
@@ -909,38 +908,17 @@ def uniform_benchmark(filepath, iterations):
     for x in range(iterations):
         print("** iteration", x)
         gen_times = []
-        pkid_times = []
-        save_times_geojson = []
-        save_times_gpkg = []
-        save_times_csv = []
         for points in points_list:
             print(f"* {x}: {points} points...")
             gen_start = time.time()
             gdf_out = points_uniform(source_gdf.loc[0, 'geometry'], points)
+            print(f"{len(gdf_out)} points generated.")
+            gdf_out.insert(0, 'PKID', range(0, len(gdf_out)))
             gen_end = time.time()
 
-            pkid_start = time.time()
-            gdf_out.insert(0, 'PKID', range(0, len(gdf_out)))
-            pkid_end = time.time()
-
-            save_geojson_start = time.time()
-            gdf_out.to_file(f"{save_dir}/QGIS_compare/RADIAN_{points}.geojson", driver='GeoJSON')
-            save_geojson_end = time.time()
-
-            save_gpkg_start = time.time()
-            gdf_out.to_file(f"{save_dir}/QGIS_compare/RADIAN_{points}.gpkg", driver='GPKG', layer='name')
-            save_gpkg_end = time.time()
-
-            save_csv_start = time.time()
-            gdf_out.to_csv(f"{save_dir}/QGIS_compare/RADIAN_{points}.csv", index=False)
-            save_csv_end = time.time()
-
             gen_times.append(gen_end - gen_start)
-            pkid_times.append(pkid_end - pkid_start)
-            save_times_geojson.append(save_geojson_end - save_geojson_start)
-            save_times_gpkg.append(save_gpkg_end - save_gpkg_start)
-            save_times_csv.append(save_csv_end - save_csv_start)
-        df = pd.DataFrame(zip([x+1 for i in range(len(points_list))], points_list, gen_times, pkid_times, save_times_geojson, save_times_gpkg, save_times_csv, [sum(i) for i in zip(gen_times, pkid_times, save_times_csv)]), columns=['experiment','points','gen_time', 'pkid_time', 'save_times_geojson', 'save_times_gpkg', 'save_times_csv', 'total_time'])
+
+        df = pd.DataFrame(zip([x+1 for i in range(len(points_list))], points_list, gen_times), columns=['experiment','points','gen_time'])
         output = pd.concat([output, df])
         print("done.")
     print("* Iterations complete.")
@@ -954,7 +932,7 @@ def uniform_benchmark(filepath, iterations):
 
     print(output)
 
-def qgis_compare(filepath, iterations=10):
+def qgis_compare(filepath, iterations=1):
     radian_data_path = f"{os.path.dirname(filepath)}/QGIS/radian_uniform_{iterations}.csv"
     if not os.path.exists(radian_data_path):
         print(f"* Running RADIAN uniform generation for {iterations} iterations...")
